9f668f9eba8510f658d24784c4a835e02ae5225a
xylophonehero <nick97000@gmail.com>
2024-12-09 22:57:57 -0400

    feat:log page

---
+++ app/logs/page.tsx
@@ -0,0 +1,40 @@
+import { Guess } from "@/components/Guess";
+import { supabase } from "@/utils/supabase/server";
+import React from "react";
+
+export default async function Logs() {
+  const { data, error } = await supabase
+    .from("games")
+    .select("*")
+    .order("id", { ascending: false })
+    .limit(50);
+
+  if (error) {
+    return <div>Error fetching logs: {error.message}</div>;
+  }
+
+  if (!data) return <div>Loading...</div>;
+
+  console.log(data);
+  return (
+    <div className="mt-10">
+      <ul className="flex flex-col gap-8">
+        {data.map((game) => (
+          <li className="flex flex-col gap-2" key={game.id}>
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <Guess
+                  key={index}
+                  word={game.guesses.split(",")[index]}
+                  code={game.scores.split(",")[index]}
+                />
+              ))}
+            <div />
+            <Guess word={game.correct_word} code="22222" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
